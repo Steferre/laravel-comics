e288a8b2094ede252a4a61ab452a0eaebd97678c
Steferre <ferrera.ste@gmail.com>
2021-06-20 12:37:00 +0200

    Completata single page bonus

--- resources/views/comicPage1.blade.php
+++ resources/views/comicPage1.blade.php
@@ -33,25 +33,90 @@
     </div>
     <div class="mainBtmHalf">
         <div class="containerMCP">
-            <div class="talentBox">
+            <div class="tableBox">
                 <h3>Talent</h3>
-                <div class="artBox">
+                <div class="rowBox">
                     <div class="boxTitle">
                         <span>Art by:</span>
                     </div>
-                    <span>
-                    @foreach($comicsList[0]['artists'] as $artist)
-                        <a href="#">{{$artist}},</a>
-                    @endforeach
-                    </span>
+                    <div class="boxList lineH">
+                        <span>
+                        @foreach($comicsList[0]['artists'] as $artist)
+                            <a href="#">{{$artist}},</a>
+                        @endforeach
+                        </span>
+                    </div>
                 </div>
-                <div class="writtenBox"></div>
+                <div class="rowBox">
+                    <div class="boxTitle">
+                        <span>Written by:</span>
+                    </div>
+                    <div class="boxList lineH">
+                        <span>
+                        @foreach($comicsList[0]['writers'] as $writer)
+                            <a href="#">{{$writer}},</a>
+                        @endforeach
+                        </span>
+                    </div>
+                </div>
             </div>
-            <div class="SpecsBox">
+            <div class="tableBox">
                 <h3>Specs</h3>
+                <div class="rowBox">
+                    <div class="boxTitle">
+                        <span>Series:</span>
+                    </div>
+                    <div class="boxList">
+                        <a href="#"><span>{{$comicsList[0]['series']}}</span></a>
+                    </div>
+                </div>
+                <div class="rowBox">
+                    <div class="boxTitle">
+                        <span>U.S. Price:</span>
+                    </div>
+                    <div class="boxList">
+                        <span>{{$comicsList[0]['price']}}</span>
+                    </div>
+                </div>
+                <div class="rowBox">
+                    <div class="boxTitle">
+                        <span>On Sale Date:</span>
+                    </div>
+                    <div class="boxList">
+                        <span>{{$comicsList[0]['sale_date']}}</span>
+                    </div>
+                </div>
             </div>                        
         </div>
 
+    </div>
+    <div class="btnBanner">
+        <div class="containerMCP">
+            <a href="#">
+                <div class="bannerCard">
+                    <span>digital comics</span>
+                    <img src="{{ asset('images/buy-comics-digital-comics.png') }}" alt="">
+                </div>
+            </a>
+            <a href="#">
+                <div class="bannerCard">
+                    <span>shop dc</span>
+                    <img src="{{ asset('images/buy-comics-merchandise.png') }}" alt="">
+                </div>
+            </a>
+            <a href="#">
+                <div class="bannerCard">
+                    <span>comic shop locator</span>
+                    <img src="{{ asset('images/buy-comics-shop-locator.png') }}" alt="">
+                </div>
+            </a>
+            <a href="#">
+                <div class="bannerCard">
+                    <span>subscriptions</span>
+                    <img src="{{ asset('images/buy-comics-subscriptions.png') }}" alt="">
+                </div>
+            </a>
+        </div>
     </div>
 </main>
 @endsection
